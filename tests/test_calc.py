d8b56ed2fd9a32214391bc6c8945147df46132ce
Ayman Elsayeed <ayman.elsayeed@mdclone.com>
2021-11-14 13:39:57 +0200

    add unit tests.

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,4 +1,5 @@
 from src.calc import add, sub
+import pytest
 
 
 def test_add():
@@ -11,3 +12,27 @@ def test_sub():
     result = sub(2, '1')
     assert result == 1
     assert 1 == sub(2, 1)
+
+
+@pytest.mark.parametrize("expected_result,a,b", [(2, 1, 1), (3, 2, 1)])
+def test_add_param(expected_result, a, b):
+    result = add(a, b)
+    assert result == expected_result
+
+
+@pytest.mark.parametrize("expected_result,a,b", [(2, 3, 1), (3, 4, 1)])
+def test_sub_param(expected_result, a, b):
+    result = sub(a, b)
+    assert result == expected_result
+
+
+def test_add_fixture(data_to_add):
+    expected_result, a, b = data_to_add
+    result = add(a, b)
+    assert result == expected_result
+
+
+def test_sub_fixture(data_to_sub):
+    expected_result, a, b = data_to_sub
+    result = sub(a, b)
+    assert result == expected_result
