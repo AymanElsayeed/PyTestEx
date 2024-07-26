0aa1ed75d1f3efde60f2c72ca8e42fa5d8442819
aymanelsayeed <ayman.elsayeed@mdclone.com>
2024-07-26 12:58:41 +0300

    add docstring.

--- src/calc.py
+++ src/calc.py
@@ -1,14 +1,37 @@
+"""
+
+calculation module
+
+"""
 
 
-def add(a: (str, int, float), b: (str, int, float)) -> (int, float):
+def add(number_one: (str, int, float), number_two: (str, int, float)) -> (int, float):
+    """
+    Add two numbers
+    :param number_one: number one
+    :type number_one: int or float
+    :param number_two: number two
+    :type number_two: int or float
+    :return: sum of two numbers
+    :rtype: int or float
+    """
     try:
-        return a+b
+        return number_one + number_two
     except TypeError:
-        return float(a) + float(b)
+        return float(number_one) + float(number_two)
 
 
-def sub(a: (str, int, float), b: (str, int, float)) -> (int, float):
+def sub(number_one: (str, int, float), number_two: (str, int, float)) -> (int, float):
+    """
+    Subtract two numbers
+    :param number_one: number one
+    :type number_one: int or float
+    :param number_two: number two
+    :type number_two: int or float
+    :return: difference of two numbers
+    :rtype: int or float
+    """
     try:
-        return a-b
+        return number_one - number_two
     except TypeError:
-        return float(a) - float(b)
+        return float(number_one) - float(number_two)
--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,14 +1,28 @@
+"""
+
+This file contains the test cases for the calc module.
+
+"""
+
 from src.calc import add, sub
 import pytest
 
 
 def test_add():
+    """
+    Test add function
+    :return: None
+    """
     result = add(1, '1')
     assert result == 2
     assert 2 == add(1, 1)
 
 
 def test_sub():
+    """
+    Test sub function
+    :return: None
+    """
     result = sub(2, '1')
     assert result == 1
     assert 1 == sub(2, 1)
@@ -16,12 +30,26 @@ def test_sub():
 
 @pytest.mark.parametrize("expected_result,a,b", [(2, 1, 1), (3, 2, 1)])
 def test_add_param(expected_result, a, b):
+    """
+    Test add function with parameters
+    :param expected_result:
+    :param a:
+    :param b:
+    :return:
+    """
     result = add(a, b)
     assert result == expected_result
 
 
 @pytest.mark.parametrize("expected_result,a,b", [(2, 3, 1), (3, 4, 1)])
 def test_sub_param(expected_result, a, b):
+    """
+    Test sub function with parameters
+    :param expected_result:
+    :param a:
+    :param b:
+    :return:
+    """
     result = sub(a, b)
     assert result == expected_result
 
